d586f5de2cb8f973dc38ca4c382aea5dea06f401
iminovvan <105599313+iminovvan@users.noreply.github.com>
2024-05-26 14:19:53 +0500

    Add files via upload

--- onlineshop/src/main/java/com/example/onlineshop/controllers/OrderController.java
+++ onlineshop/src/main/java/com/example/onlineshop/controllers/OrderController.java
@@ -6,6 +6,7 @@ import com.example.onlineshop.entities.Order;
 import com.example.onlineshop.services.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,6 +18,7 @@ public class OrderController {
     private OrderService orderService;
 
     @PostMapping
+    //@PreAuthorize("hasAuthority('ADMIN') or hasAuthority('CUSTOMER')")
     public ResponseEntity<OrderDto> createOrder(@RequestBody OrderDto orderDto) {
         return ResponseEntity.ok(orderService.createOrder(orderDto));
     }
