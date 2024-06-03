bf3a69311f3ec73920a9af52e23feb44dcc4c2fb
iminovvan <105599313+iminovvan@users.noreply.github.com>
2024-06-03 20:38:01 +0500

    Updated SecurityFilterChain: added jwt authentication

--- onlineshop/src/main/java/com/example/onlineshop/config/SecurityConfig.java
+++ onlineshop/src/main/java/com/example/onlineshop/config/SecurityConfig.java
@@ -1,28 +1,28 @@
 package com.example.onlineshop.config;
 
-import com.example.onlineshop.config.security.CustomAccessDeniedHandler;
-import com.example.onlineshop.config.security.UserDetailsServiceImpl;
+import com.example.onlineshop.config.security.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.AccessDeniedHandler;
-
-import static org.springframework.security.config.Customizer.withDefaults;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+   private final JwtAuthenticationFilter jwtAuthFilter;
+    private final AuthenticationProvider authenticationProvider;
     @Autowired
     private AccessDeniedHandler customAccessDeniedHandler;
 
@@ -30,47 +30,26 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
-                .httpBasic(Customizer.withDefaults())
-                .authorizeHttpRequests(auth->auth
-                        // products/all + products/{id} are public
-                        .requestMatchers(HttpMethod.GET, "/", "/products/**").permitAll()
-                        // users/all + users/{id} are accessible to ADMINS
-                        .requestMatchers(HttpMethod.GET,"/users/**", "/orders/**").hasRole("ADMIN")
-                        // only ADMINS can create users
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.POST, "/users/register").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/users/authenticate").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/home", "/products/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/demo").authenticated()
+                        .requestMatchers(HttpMethod.GET, "/users/**", "/orders/**").hasRole("ADMIN")
                         .requestMatchers(HttpMethod.POST, "/users", "/products", "/orders").hasRole("ADMIN")
-                        // only ADMINS can update users, get/create/update orders
                         .requestMatchers(HttpMethod.PUT, "/users/**", "/orders/**", "/products/**").hasRole("ADMIN")
                         .requestMatchers(HttpMethod.DELETE, "/orders/**", "/users/**", "/products/**").hasRole("ADMIN")
-                        .anyRequest().authenticated()) //default
-                .formLogin(withDefaults())
+                        .anyRequest().authenticated())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
                 .exceptionHandling(exceptionHandling ->
                         exceptionHandling.accessDeniedHandler(customAccessDeniedHandler));
         return http.build();
     }
 
     @Bean
-    public AuthenticationProvider authenticationProvider() {
-        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
-        daoAuthenticationProvider.setUserDetailsService(userDetailsService());
-        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
-        return daoAuthenticationProvider;
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
-
-    @Bean
-    public PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    public UserDetailsService userDetailsService(){
-        return new UserDetailsServiceImpl();
-    }
-
-    @Bean
-    public AccessDeniedHandler accessDeniedHandler() {
-        return new CustomAccessDeniedHandler();
-    }
-
-
-
 }
