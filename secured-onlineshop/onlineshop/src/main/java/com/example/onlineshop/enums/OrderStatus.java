8485f8b7d69e21356510fabc2fe85476fc737d30
iminovvan <105599313+iminovvan@users.noreply.github.com>
2024-06-04 11:04:26 +0500

    Delete secured-onlineshop directory

--- secured-onlineshop/onlineshop/src/main/java/com/example/onlineshop/enums/OrderStatus.java
+++
@@ -1,7 +0,0 @@
-package com.example.onlineshop.enums;
-
-public enum OrderStatus {
-    WAITING,
-    PAID,
-    CANCELED
-}
--- secured-onlineshop/onlineshop/src/main/java/com/example/onlineshop/repositories/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.onlineshop.repositories;
-
-import com.example.onlineshop.entities.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
